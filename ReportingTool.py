cad2a828ff373a0d703794ea3de5435a7fb57bdb
Shiva <dgpshiva@gmail.com>
2017-12-31 21:17:02 -0600

    Adding changes to make code pep8 compatible

--- ReportingTool.py
+++ ReportingTool.py
@@ -9,13 +9,13 @@ def getTopThreeMostPopularArticles():
     conn = psycopg2.connect(database=dbName)
     cursor = conn.cursor()
     query = '''
-                SELECT title, count(*) AS NumberOfViews
-                FROM articles
-                INNER JOIN SuccessfulllyReadArticlesView
-                ON articles.slug = SuccessfulllyReadArticlesView.slug
-                GROUP BY title
-                ORDER BY NumberOfViews DESC
-                LIMIT 3;
+        SELECT title, count(*) AS NumberOfViews
+        FROM articles
+        INNER JOIN SuccessfulllyReadArticlesView
+        ON articles.slug = SuccessfulllyReadArticlesView.slug
+        GROUP BY title
+        ORDER BY NumberOfViews DESC
+        LIMIT 3;
             '''
     cursor.execute(query)
     rows = cursor.fetchall()
@@ -30,22 +30,23 @@ def mostPopularArticleAuthors():
     conn = psycopg2.connect(database=dbName)
     cursor = conn.cursor()
     query = '''
-                SELECT authors.name,
-                       sum(TitleAndNumberOfViews.NumberOfViews) AS TotalViewPerAuthor
-                FROM authors
-                INNER JOIN articles
-                ON authors.id = articles.author
-                INNER JOIN
-                (
-                    SELECT title, count(*) AS NumberOfViews
-                    FROM articles
-                    INNER JOIN SuccessfulllyReadArticlesView
-                    ON articles.slug = SuccessfulllyReadArticlesView.slug
-                    GROUP BY title
-                ) AS TitleAndNumberOfViews
-                ON articles.title = TitleAndNumberOfViews.title
-                GROUP BY authors.name
-                ORDER BY TotalViewPerAuthor DESC;
+        SELECT authors.name,
+        sum(TitleAndNumberOfViews.NumberOfViews)
+        AS TotalViewPerAuthor
+        FROM authors
+        INNER JOIN articles
+        ON authors.id = articles.author
+        INNER JOIN
+        (
+            SELECT title, count(*) AS NumberOfViews
+            FROM articles
+            INNER JOIN SuccessfulllyReadArticlesView
+            ON articles.slug = SuccessfulllyReadArticlesView.slug
+            GROUP BY title
+        ) AS TitleAndNumberOfViews
+        ON articles.title = TitleAndNumberOfViews.title
+        GROUP BY authors.name
+        ORDER BY TotalViewPerAuthor DESC;
             '''
     cursor.execute(query)
     rows = cursor.fetchall()
@@ -60,26 +61,31 @@ def lotOfErrorsDays():
     conn = psycopg2.connect(database=dbName)
     cursor = conn.cursor()
     query = '''
-                SELECT
-                    to_char(dateAndTotalCounts.theDate, 'Mon DD, YYYY'),
-                    (CAST (errorCounts AS FLOAT)/CAST(totalCounts AS FLOAT))
-                      * 100 AS errorPercentage
-                FROM
-                (
-                    SELECT time::timestamp::date AS theDate, count(*) AS errorCounts
-                    FROM log
-                    WHERE status != '200 OK'
-                    GROUP BY time::timestamp::date
-                ) AS dateAndErrorCounts
-                INNER JOIN
-                (
-                    SELECT time::timestamp::date AS theDate, count(*) AS totalCounts
-                    FROM log
-                    GROUP BY time::timestamp::date
-                ) AS dateAndTotalCounts
-                ON dateAndErrorCounts.theDate = dateAndTotalCounts.theDate
-                WHERE ( CAST (errorCounts AS FLOAT) / CAST(totalCounts AS FLOAT) )  * 100 > 1
-                ORDER BY ( CAST (errorCounts AS FLOAT) / CAST(totalCounts AS FLOAT) )  * 100 DESC, dateAndTotalCounts.theDate;
+        SELECT
+        to_char(dateAndTotalCounts.theDate, 'Mon DD, YYYY'),
+        (CAST (errorCounts AS FLOAT)/CAST(totalCounts AS FLOAT)) * 100
+        AS errorPercentage
+        FROM
+        (
+            SELECT time::timestamp::date AS theDate,
+                    count(*) AS errorCounts
+            FROM log
+            WHERE status != '200 OK'
+            GROUP BY time::timestamp::date
+        ) AS dateAndErrorCounts
+        INNER JOIN
+        (
+            SELECT time::timestamp::date AS theDate, count(*)
+            AS totalCounts
+            FROM log
+            GROUP BY time::timestamp::date
+        ) AS dateAndTotalCounts
+        ON dateAndErrorCounts.theDate = dateAndTotalCounts.theDate
+        WHERE
+        (CAST (errorCounts AS FLOAT)/CAST(totalCounts AS FLOAT))*100 > 1
+        ORDER BY
+        (CAST (errorCounts AS FLOAT)/CAST(totalCounts AS FLOAT))*100 DESC,
+        dateAndTotalCounts.theDate;
             '''
     cursor.execute(query)
     rows = cursor.fetchall()
